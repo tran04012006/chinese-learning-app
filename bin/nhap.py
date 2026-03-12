02e85a62af8414098053c7a18c8c504981cc0abf
tran04012006 <bichtranngo416@gmail.com>
2026-03-12 21:04:04 +0700

    src

--- bin/nhap.py
+++
@@ -1,8 +0,0 @@
-import google.generativeai as genai
-
-genai.configure(api_key="AIzaSyBU8FsGS4Dm9Bi7CPslztYY04WV6O0cVz4")
-
-for m in genai.list_models():
-    if 'generateContent' in m.supported_generation_methods:
-        print(f"Model Name: {m.name}") # Đây là ID bạn cần điền vào code
-
--- bin/tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def min (self, nums: List[int]) -> int:
-        runningSum = 0
-        minSum = 0
-        for num in nums:
-            runningSum 
